a50820829cb79dae063b9827356101a3a4aafc2e
LenyPython <buniadyserv@gmail.com>
2021-11-10 17:16:11 +0100

    Added custom btns. Changed open close to mouseover and moue leave

--- src/components/ThemeChanger/Controls.tsx
+++ src/components/ThemeChanger/Controls.tsx
@@ -1,37 +1,55 @@
 import styled from 'styled-components'
-import {Dispatch, SetStateAction} from 'react'
+import ThemeButton from '../../styled/ThemeButton'
+import RangeSlider from '../../styled/RangeSlider'
 import {useAppDispatch, useAppSelector} from '../../app/hooks'
 import {getGlobalStyle, setDarkTheme, setHue, setLightTheme} from '../../slices/GlobalStyleSlice'
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  & > * {
+    margin-bottom: 1em;
+  }
 `
-interface Props {
-  setIsOpen: Dispatch<SetStateAction<boolean>>
-}
-const Controls: React.FC<Props> = ({ setIsOpen }) => {
-  const { hue } = useAppSelector(getGlobalStyle)
+
+
+const Controls = () => {
+  const { hue, saturation } = useAppSelector(getGlobalStyle)
   const dispatch = useAppDispatch()
-  const handleClick = () => setIsOpen(false)
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const hue = e.target.value
     dispatch(setHue(+hue))
   }
   return(
-    <Container>
-      <button onClick={()=>dispatch(setLightTheme())}>light</button>
-      <button onClick={()=>dispatch(setDarkTheme())}>dark</button>
-      <input 
-      type="range"
-      min={0}
-      max={360}
+    <Container >
+      <div>
+      <ThemeButton 
+        onClick={()=>dispatch(setLightTheme())}
+        style={{
+          color: `hsl(${hue},${saturation}%,40%)`,
+          backgroundColor: `hsl(${hue},${saturation}%,85%)`,
+        }}
+      >
+        light
+      </ThemeButton>
+      <ThemeButton
+        onClick={()=>dispatch(setDarkTheme())}
+          style={{
+          color: `hsl(${hue},${saturation}%,60%)`,
+          backgroundColor: `hsl(${hue},${saturation}%,1%)`
+
+        }}
+      >
+        dark
+      </ThemeButton>
+      </div>
+      <RangeSlider 
+      min='0'
+      max='360'
       onChange={handleChange}
       value={hue}
       />
 
-      <button onClick={handleClick}>cancel</button>
-      <button onClick={handleClick}>apply</button>
     </Container>
   )
 }
