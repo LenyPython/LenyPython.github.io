39208669016698697bdedb6857173bb1bfe05b8a
LenyPython <buniadyserv@gmail.com>
2021-11-12 22:18:40 +0100

    Adding simple sphere animation

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,11 +1,44 @@
+import styled from 'styled-components'
+import * as THREE from 'three'
+import { Suspense, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 
+const CanvasContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
 
+function Globe(props: JSX.IntrinsicElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  useFrame((state, delta) => {
+    ref.current.rotation.x += 0.01
+  })
+  return (
+    <>
+      <ambientLight intensity={1}/>
+    <mesh
+      {...props}
+      ref={ref}
+    >
+      <sphereGeometry args={[1, 32, 16]} />
+      <meshMatcapMaterial color={'hotpink'} />
+    </mesh>
+    </>
+  )
+}
 
 const Portfolio = () => {
-  return(
-    <h1>Portfolio</h1>
-  )
+
+  return <CanvasContainer>
+  <Canvas>
+    <Suspense fallback={null} />
+    <OrbitControls />
+    <Globe />
+  </Canvas>
+
+  </CanvasContainer>
 }
 
 export default Portfolio
