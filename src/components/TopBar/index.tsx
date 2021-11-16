505e8c4ffe12c4221e1cfa6c0d8a3b958447cc49
LenyPython <buniadyserv@gmail.com>
2021-11-16 18:54:06 +0100

    Changed Top Bar styles from bg colot to container color. Alsdo added z-indexes

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -6,8 +6,11 @@ const Bar = styled.div`
   left: 0;
   width: 100%;
   height: 3%;
-  background: var(--main-font-color);
-  box-shadow: 0 0 5px var(--main-font-color);
+  background: var(--main-containers-color);
+  z-index: 1;
+  box-shadow: 0 0 5px var(--main-font-color),
+              0 0 15px var(--main-font-color),
+              0 0 20px var(--main-font-color);
   &::after {
     position: absolute;
     content: '';
@@ -17,8 +20,9 @@ const Bar = styled.div`
     width: 250px;
     height: 100px;
     border-radius: 100%;
-    background: var(--main-background-color);
-    box-shadow: 0 0 5px var(--main-background-color);
+    border-top: 7px solid var(--main-font-color);
+    background: var(--main-containers-color);
+    box-shadow: inset 0 0 5px var(--main-font-color);
   }
 `
 
