fe94c2028d531ce413ce39b2cb328f6cbab01941
LenyPython <buniadyserv@gmail.com>
2021-11-18 19:19:48 +0100

    Removed useless html from glob animation. Added sun mesh to Thre background with custom maping

--- src/components/Portfolio/ThreeBackground.tsx
+++ src/components/Portfolio/ThreeBackground.tsx
@@ -1,12 +1,32 @@
-import Spot from '../ThreeModels/Spot'
 import { Globe } from '../ThreeModels'
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { Suspense, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, useTexture } from '@react-three/drei'
 import {useAppSelector} from '../../app/hooks'
 import {getGlobalStyle} from '../../slices/GlobalStyleSlice'
 import {Color} from 'three'
+import rays from '../../textures/2k_sun.jpg'
 
+const Sun = (props: { color: string } & JSX.IntrinsicElements['mesh']) => {
+  const { color } = props
+  const ref = useRef<THREE.Mesh>(null!)
+  const sunMap = useTexture(rays)
+  useFrame(()=>{
+    let rot = ref.current.rotation
+    rot.y -= 0.01
+  })
+  return (
+      <mesh ref={ref}>
+          <sphereGeometry args={[.4, 16, 16]} />
+          <meshStandardMaterial 
+            emissive={ new Color(color) }
+            emissiveMap={sunMap}
+            alphaMap={sunMap}
+          />
+        </mesh>
+  )
+
+}
 
 const ThreeBackground = () => {
   const { hue, saturation, light }  = useAppSelector(getGlobalStyle)
@@ -17,16 +37,8 @@ const ThreeBackground = () => {
     <Suspense fallback={null}>
     <OrbitControls enablePan={false} />
     <pointLight color={color} />
-    <ambientLight color='white' />
     <Globe />
-    <Spot />
-      <mesh >
-          <sphereGeometry args={[.4, 16, 16]} />
-          <meshStandardMaterial 
-            emissive={new Color(color)}
-            metalness={0.1}
-          />
-        </mesh>
+    <Sun color={color} />
       </Suspense>
   </Canvas>
 
--- src/components/ThreeModels/Globe.tsx
+++ src/components/ThreeModels/Globe.tsx
@@ -1,7 +1,6 @@
 import * as THREE from 'three'
 import map from '../../textures/transparent.png'
 import alphaMap from '../../textures/inv.jpg'
-import Projects from '../Projects'
 import { useTexture, Html } from "@react-three/drei"
 import { extend, useFrame } from "@react-three/fiber"
 import { useRef } from "react"
@@ -13,7 +12,6 @@ const Globe = (props: JSX.IntrinsicElements['mesh']) => {
     map,
     alphaMap
   })
-  const globeRef = useRef<THREE.Mesh>(null!)
   const ref = useRef<THREE.Mesh>(null!)
   useFrame(()=> {
     let rot = ref.current.rotation
@@ -22,8 +20,7 @@ const Globe = (props: JSX.IntrinsicElements['mesh']) => {
   })
 
   return (
-    <group ref={ref} position={[-1, 0, -2]}>
-    <mesh ref={globeRef}
+    <mesh ref={ref}
       {...props}
     >
       <sphereGeometry args={[2, 32, 32]} />
@@ -33,35 +30,6 @@ const Globe = (props: JSX.IntrinsicElements['mesh']) => {
         {...textures}
       />
     </mesh>
-    <mesh position={[0, 0, 2.4]} rotation={[0, 0, 0]}>
-      <Html
-        transform 
-        zIndexRange={[1, 0]}
-        occlude={[globeRef]} 
-        ><Projects /></Html>
-    </mesh>
-    <mesh position={[1.5, 0, 1.6]} rotation={[0, Math.PI/5, 0]}>
-      <Html
-        transform 
-        zIndexRange={[1, 0]}
-        occlude={[globeRef]} 
-        > <h1>helo</h1></Html>
-    </mesh>
-    <mesh position={[-2, -1, 1.1]} rotation={[Math.PI / 10, -Math.PI / 3, Math.PI/15]}>
-      <Html
-        transform 
-        zIndexRange={[1, 0]}    
-        occlude={[globeRef]} 
-        > <h1>helo</h1></Html>
-    </mesh>
-    <mesh position={[0, 1, 2.1]} rotation={[-Math.PI/10 , 0, 0]}>
-      <Html
-        transform 
-        zIndexRange={[1, 0]}    
-        occlude={[globeRef]} 
-        > <h1>helo</h1></Html>
-    </mesh>
-    </group>
   )
 }
 
