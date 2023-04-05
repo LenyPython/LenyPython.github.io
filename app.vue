45959d7420035daf7c46e5b2b757ff0fe19fd65d
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2023-04-06 00:07:55 +0200

    Storybook setup with autoimports

---
+++ app.vue
@@ -0,0 +1,11 @@
+
+import { AppNavbar } from './.nuxt/components';
+
+<template>
+    <h1>component</h1>
+    <div>
+        <My />
+        <AppNavbar />
+        <AppFooter />
+    </div>
+</template>
