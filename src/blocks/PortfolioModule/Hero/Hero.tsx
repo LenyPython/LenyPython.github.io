e95e048bedaf28a4c9344f17a24a7eaf184acff8
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2023-08-05 23:54:12 +0200

    More updates

--- src/blocks/PortfolioModule/Hero/Hero.tsx
+++ src/blocks/PortfolioModule/Hero/Hero.tsx
@@ -1,7 +1,12 @@
-import RichText from '@/components/RIchText/RichText'
-import { Image } from '@/types/global'
 import { ISbRichtext, renderRichText } from '@storyblok/react'
 import Link from 'next/link'
+import Image from 'next/image'
+
+import { ImageType } from '@/types/global'
+
+import RichText from '@/components/RIchText/RichText'
+
+import HeroImg from '@/assets/images/hero.jpg'
 
 type Props = {
   blok: {
@@ -9,19 +14,21 @@ type Props = {
     rich_text: ISbRichtext
     cta_link: string
     headline: string
-    Image: Image
+    Image: ImageType
   }
 }
 const Hero: React.FC<Props> = ({ blok }) => {
-  const { headline, Image, rich_text, cta_link, call_to_action } = blok
+  const { headline, Image: img, rich_text, cta_link, call_to_action } = blok
   return (
     <div className='flex '>
-      <div className='flex flex-col text-green-500'>
-        <h2>{headline}</h2>
-        <RichText html={renderRichText(rich_text)} />
+      <div className='flex flex-col justify-around text-green-500'>
+        <div>
+          <h2 className='text-6xl font-bold mb-10'>{headline}</h2>
+          <RichText html={renderRichText(rich_text)} />
+        </div>
         <Link href={cta_link}>{call_to_action}</Link>
       </div>
-      <img src={Image.filename} alt={Image.alt} />
+      <Image src={img.filename} width={500} height={500} alt={img.alt} />
     </div>
   )
 }
--- src/blocks/PortfolioModule/Section/Section.tsx
+++ src/blocks/PortfolioModule/Section/Section.tsx
@@ -2,7 +2,11 @@ import { StoryblokComponent, storyblokEditable } from '@storyblok/react'
 
 const Section: React.FC<{ blok: any }> = ({ blok }) => {
   return (
-    <section id={blok.ID} className='min-h-[80vh]' {...storyblokEditable(blok)}>
+    <section
+      id={blok.ID}
+      className='flex justify-center items-center min-h-[80vh]'
+      {...storyblokEditable(blok)}
+    >
       {blok.Blocks.map((blok: any) => (
         <StoryblokComponent blok={blok} key={blok._id} />
       ))}
