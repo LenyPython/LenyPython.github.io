2d511501169230b2b9fb943a91117c3075bc9e38
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2023-08-08 23:48:36 +0200

    Add technology tree

--- src/PortfolioModule/blocks/TechGroup/TechGroup.tsx
+++ src/PortfolioModule/blocks/TechGroup/TechGroup.tsx
@@ -1,5 +1,23 @@
-const TechGroup = () => {
-  return <div>TechGroup</div>
+import { StoryblokComponent } from '@storyblok/react'
+
+type Props = {
+  blok: {
+    headline: string
+    techs: any
+  }
+}
+
+const TechGroup: React.FC<Props> = ({ blok }) => {
+  return (
+    <div>
+      <div>
+        <h2>{blok.headline}</h2>
+        {blok.techs.map((component: any) => (
+          <StoryblokComponent blok={component} key={component._uid} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default TechGroup
