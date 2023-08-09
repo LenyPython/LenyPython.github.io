a0f240e213bf8dbbc090fcd9b56b55d7dc3d923f
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2023-08-09 23:24:10 +0200

    work on making svg images

--- src/PortfolioModule/blocks/Tech/Tech.tsx
+++ src/PortfolioModule/blocks/Tech/Tech.tsx
@@ -1,5 +1,29 @@
-const Tech = () => {
-  return <div>Tech</div>
+import { log } from 'console'
+import Image from 'next/image'
+
+type Props = {
+  blok: {
+    svg: {
+      id: number
+      _uid: string
+      alt: string
+      title: string
+      filename: string
+    }
+  }
+}
+const Tech: React.FC<Props> = ({ blok }) => {
+  const { svg } = blok
+  console.log(svg)
+  return (
+    <Image
+      src={svg.filename}
+      width={30}
+      height={30}
+      title={svg.title}
+      alt={svg.alt}
+    />
+  )
 }
 
 export default Tech
--- src/PortfolioModule/blocks/TechGroup/TechGroup.tsx
+++ src/PortfolioModule/blocks/TechGroup/TechGroup.tsx
@@ -3,7 +3,8 @@ import { StoryblokComponent } from '@storyblok/react'
 type Props = {
   blok: {
     headline: string
-    techs: any
+    advanced_tech: any
+    basic_tech: any
   }
 }
 
@@ -11,7 +12,10 @@ const TechGroup: React.FC<Props> = ({ blok }) => {
   return (
     <div className='w-full p-2 border-y border-y-font mb-3'>
       <h2>{blok.headline}</h2>
-      {blok.techs.map((component: any) => (
+      {blok.advanced_tech.map((component: any) => (
+        <StoryblokComponent blok={component} key={component._uid} />
+      ))}
+      {blok.basic_tech.map((component: any) => (
         <StoryblokComponent blok={component} key={component._uid} />
       ))}
     </div>
