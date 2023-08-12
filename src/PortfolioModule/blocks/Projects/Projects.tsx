a68d5fb7f1b4566121957144e59d27b524d7c291
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2023-08-12 20:21:57 +0200

    Add filters to projects

--- src/PortfolioModule/blocks/Projects/Projects.tsx
+++ src/PortfolioModule/blocks/Projects/Projects.tsx
@@ -1,17 +1,78 @@
-import { ProjectsStoryblok } from '@/types/component-types-sb'
+import { ProjectStoryblok, ProjectsStoryblok } from '@/types/component-types-sb'
+import { SvgType } from '@/utils/SvgProvider'
 import { StoryblokComponent } from '@storyblok/react'
+import { useState } from 'react'
 
 type Props = {
   blok: ProjectsStoryblok
 }
+enum role {
+  any = 'any',
+  dev = 'developer',
+  main = 'maintenance',
+  sup = 'support'
+}
 
 const Projects: React.FC<Props> = ({ blok }) => {
+  let { projects } = blok
+  const technologies: SvgType[] = Object.values(SvgType)
+  const [roleFilter, setRoleFilter] = useState(role.any)
+  const onRoleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setRoleFilter(e.target.value as role)
+  }
+  const [techFilter, setTechFilter] = useState([] as SvgType[])
+  const onTechChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setTechFilter(e.target.value)
+    console.log(e.target.value)
+    console.log(techFilter)
+  }
+  if (roleFilter !== role.any)
+    projects = projects.filter((proj: ProjectStoryblok) =>
+      proj.role.includes(roleFilter)
+    )
   return (
     <div className='relative w-3/4 max-w-4xl p-10 rounded-xl bg-black backdrop-blur-lg '>
       <h2 className='absolute top-0 left-8 -translate-y-1/2 text-3xl'>
         {blok.headline}
       </h2>
-      {blok.projects.map((project: any) => (
+      <div className='w-full flex mb-5'>
+        <label htmlFor='role' className='border-2 pl-1 mr-1 bg-background'>
+          Role:
+          <select
+            className='border-0 outline-0 mx-1 px-2 bg-background'
+            onChange={onRoleChange}
+            value={roleFilter}
+            name='role'
+            id='role'
+          >
+            <option selected value={role.any}>
+              {role.any}
+            </option>
+            <option value={role.dev}>{role.dev}</option>
+            <option value={role.main}>{role.main}</option>
+            <option value={role.sup}>{role.sup}</option>
+          </select>
+        </label>
+        <label htmlFor='technology' className='border-2 pl-1 bg-background'>
+          technology:
+          <select
+            className='border-0 outline-0 mx-1 px-2 bg-background'
+            name='technology'
+            onChange={onTechChange}
+            id='technology'
+          >
+            <option selected value={role.any}>
+              {role.any}
+            </option>
+            {technologies.map((tech: SvgType) => (
+              <option key={tech} value={tech}>
+                {tech}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+      {projects.map((project: any) => (
         <StoryblokComponent blok={project} key={project._uid} />
       ))}
     </div>
--- src/types/component-types-sb.d.ts
+++ src/types/component-types-sb.d.ts
@@ -1,145 +1,145 @@
-import {StoryblokStory} from 'storyblok-generate-ts'
+import { StoryblokStory } from 'storyblok-generate-ts'
 
 export interface AssetStoryblok {
-  alt?: string;
-  copyright?: string;
-  id: number;
-  filename: string;
-  name: string;
-  title?: string;
-  focus?: string;
-  [k: string]: any;
+  alt?: string
+  copyright?: string
+  id: number
+  filename: string
+  name: string
+  title?: string
+  focus?: string
+  [k: string]: any
 }
 
 export interface RichtextStoryblok {
-  type: string;
-  content?: RichtextStoryblok[];
-  marks?: RichtextStoryblok[];
-  attrs?: any;
-  text?: string;
-  [k: string]: any;
+  type: string
+  content?: RichtextStoryblok[]
+  marks?: RichtextStoryblok[]
+  attrs?: any
+  text?: string
+  [k: string]: any
 }
 
 export type MultilinkStoryblok =
   | {
-      cached_url?: string;
-      linktype?: string;
-      [k: string]: any;
+      cached_url?: string
+      linktype?: string
+      [k: string]: any
     }
   | {
-      id?: string;
-      cached_url?: string;
-      anchor?: string;
-      linktype?: "story";
+      id?: string
+      cached_url?: string
+      anchor?: string
+      linktype?: 'story'
       story?: {
-        name: string;
-        created_at?: string;
-        published_at?: string;
-        id: number;
-        uuid: string;
+        name: string
+        created_at?: string
+        published_at?: string
+        id: number
+        uuid: string
         content?: {
-          [k: string]: any;
-        };
-        slug: string;
-        full_slug: string;
-        sort_by_date?: null | string;
-        position?: number;
-        tag_list?: string[];
-        is_startpage?: boolean;
-        parent_id?: null | number;
+          [k: string]: any
+        }
+        slug: string
+        full_slug: string
+        sort_by_date?: null | string
+        position?: number
+        tag_list?: string[]
+        is_startpage?: boolean
+        parent_id?: null | number
         meta_data?: null | {
-          [k: string]: any;
-        };
-        group_id?: string;
-        first_published_at?: string;
-        release_id?: null | number;
-        lang?: string;
-        path?: null | string;
-        alternates?: any[];
-        default_full_slug?: null | string;
-        translated_slugs?: null | any[];
-        [k: string]: any;
-      };
-      [k: string]: any;
+          [k: string]: any
+        }
+        group_id?: string
+        first_published_at?: string
+        release_id?: null | number
+        lang?: string
+        path?: null | string
+        alternates?: any[]
+        default_full_slug?: null | string
+        translated_slugs?: null | any[]
+        [k: string]: any
+      }
+      [k: string]: any
     }
   | {
-      url?: string;
-      cached_url?: string;
-      anchor?: string;
-      linktype?: "asset" | "url";
-      [k: string]: any;
+      url?: string
+      cached_url?: string
+      anchor?: string
+      linktype?: 'asset' | 'url'
+      [k: string]: any
     }
   | {
-      email?: string;
-      linktype?: "email";
-      [k: string]: any;
-    };
+      email?: string
+      linktype?: 'email'
+      [k: string]: any
+    }
 
 export interface HeroStoryblok {
-  Image?: AssetStoryblok;
-  headline?: string;
-  rich_text?: RichtextStoryblok;
-  call_to_action?: string;
-  cta_link: MultilinkStoryblok;
-  _uid: string;
-  component: "Hero";
-  [k: string]: any;
+  Image?: AssetStoryblok
+  headline?: string
+  rich_text?: RichtextStoryblok
+  call_to_action?: string
+  cta_link: MultilinkStoryblok
+  _uid: string
+  component: 'Hero'
+  [k: string]: any
 }
 
 export interface PortfolioContentLayoutStoryblok {
-  Sections: SectionStoryblok[];
-  _uid: string;
-  component: "PortfolioContentLayout";
-  [k: string]: any;
+  Sections: SectionStoryblok[]
+  _uid: string
+  component: 'PortfolioContentLayout'
+  [k: string]: any
 }
 
 export interface ProjectStoryblok {
-  project_name: string;
-  role: ("" | "developer" | "maintenance" | "support")[];
-  description?: RichtextStoryblok;
-  tech: TechStoryblok[];
-  live_page?: MultilinkStoryblok;
-  _uid: string;
-  component: "Project";
-  [k: string]: any;
+  project_name: string
+  role: ('developer' | 'maintenance' | 'support')[]
+  description?: RichtextStoryblok
+  tech: TechStoryblok[]
+  live_page?: MultilinkStoryblok
+  _uid: string
+  component: 'Project'
+  [k: string]: any
 }
 
 export interface ProjectsStoryblok {
-  headline?: string;
-  projects: any[];
-  _uid: string;
-  component: "Projects";
-  [k: string]: any;
+  headline?: string
+  projects: any[]
+  _uid: string
+  component: 'Projects'
+  [k: string]: any
 }
 
 export interface SectionStoryblok {
-  ID?: string;
-  Blocks: any[];
-  _uid: string;
-  component: "Section";
-  [k: string]: any;
+  ID?: string
+  Blocks: any[]
+  _uid: string
+  component: 'Section'
+  [k: string]: any
 }
 
 export interface TechStoryblok {
-  svg_type: string;
-  _uid: string;
-  component: "Tech";
-  [k: string]: any;
+  svg_type: string
+  _uid: string
+  component: 'Tech'
+  [k: string]: any
 }
 
 export interface TechGroupStoryblok {
-  headline?: string;
-  advanced_tech?: TechStoryblok[];
-  basic_tech?: TechStoryblok[];
-  _uid: string;
-  component: "TechGroup";
-  [k: string]: any;
+  headline?: string
+  advanced_tech?: TechStoryblok[]
+  basic_tech?: TechStoryblok[]
+  _uid: string
+  component: 'TechGroup'
+  [k: string]: any
 }
 
 export interface TechnologyStoryblok {
-  headline: string;
-  tech_groups: TechGroupStoryblok[];
-  _uid: string;
-  component: "Technology";
-  [k: string]: any;
+  headline: string
+  tech_groups: TechGroupStoryblok[]
+  _uid: string
+  component: 'Technology'
+  [k: string]: any
 }
