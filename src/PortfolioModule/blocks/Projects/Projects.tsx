873db2993946b2fa1918262d69843dbfbdb68895
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2023-09-25 23:56:01 +0200

    Refator code to smaller comonent, not working filters

--- src/PortfolioModule/blocks/Projects/Projects.tsx
+++ src/PortfolioModule/blocks/Projects/Projects.tsx
@@ -6,37 +6,21 @@ import {
 	LangEnum,
 	TechEnum
 } from '@/Global/components/SvgProvider/SvgProvider'
+import ProjectFilters, {
+	projType,
+	role,
+	technologiesEnum
+} from '@/PortfolioModule/components/ProjectsFilters'
 
 type Props = {
 	blok: ProjectsStoryblok
 }
 
-type technologiesEnum = LangEnum & DevEnum & TechEnum
-
-enum role {
-	any = 'any',
-	dev = 'developer',
-	main = 'maintenance',
-	sup = 'support'
-}
-enum projType {
-	any = 'any',
-	commercial = 'commercial',
-	side = 'side'
-}
-
 const Projects: React.FC<Props> = ({ blok }) => {
 	let { projects } = blok
-	const [isTechInputOpen, setTechInputOpen] = useState(false)
-	const technologies: technologiesEnum[] = [
-		...Object.values(LangEnum),
-		...Object.values(DevEnum),
-		...Object.values(TechEnum)
-	]
 	const [roleFilter, setRoleFilter] = useState(role.any)
 	const [techFilter, setTechFilter] = useState(new Set<TechEnum>())
 	const [typeFilter, setTypeFilter] = useState(projType.any)
-	const toggleTechInput = () => setTechInputOpen(prev => !prev)
 	const onRoleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
 		setRoleFilter(e.target.value as role)
 	}
@@ -68,84 +52,15 @@ const Projects: React.FC<Props> = ({ blok }) => {
 			})
 		)
 	}
-	const labelStyles = 'flex justify-between p-1 m-0.5 text-center md:w-2/5'
-	const selectStyles =
-		'border border-font outline-0 w-1/2 mx-1 px-2 bg-background'
 	return (
 		<div className='relative flex flex-col items-center  w-3/4 max-w-4xl'>
 			<h2 className='absolute top-0 left-8 -translate-y-3/4 text-3xl z-10'>
 				{blok.headline}
 			</h2>
-			<div className='sticky top-0 flex flex-col mb-5 px-5 py-2 w-full shadow-main shadow-font rounded-xl backdrop-blur-lg md:top-16 md:px-10 z-10'>
-				<div className='flex flex-col w-full md:flex-row md:items-center md:justify-around'>
-					<label htmlFor='work' className={labelStyles}>
-						Type:
-						<select
-							className={selectStyles}
-							onChange={onTypeChange}
-							value={typeFilter}
-							name='work'
-							id='work'
-						>
-							<option value={projType.any}>{projType.any}</option>
-							<option value={projType.commercial}>{projType.commercial}</option>
-							<option value={projType.side}>{projType.side}</option>
-						</select>
-					</label>
-					<label htmlFor='role' className={labelStyles}>
-						Role:
-						<select
-							className={selectStyles}
-							onChange={onRoleChange}
-							value={roleFilter}
-							name='role'
-							id='role'
-						>
-							{Object.values(role).map((role: string) => (
-								<option value={role} key={`role-${role}`}>
-									{role}
-								</option>
-							))}
-						</select>
-					</label>
-				</div>
-				<div className='relative w-full border-font border-2 py-1 bg-background text-center'>
-					<button onClick={toggleTechInput}>
-						{isTechInputOpen ? (
-							'Search'
-						) : (
-							<>
-								Tech Stack:{' '}
-								{techFilter.size > 0
-									? techFilter.size === 1
-										? techFilter.values().next().value
-										: techFilter.size
-									: 'all'}
-							</>
-						)}
-					</button>
-					{isTechInputOpen && (
-						<fieldset className='absolute top-[105%] left-0 right-0 bg-background text-center p-3 grid grid-cols-1 sm:text-left sm:gap-2 sm:grid-cols-3 lg:grid-cols-5'>
-							{technologies.map((tech: technologiesEnum) => {
-								return (
-									<label key={`tech-${tech}`} htmlFor={tech} className='m-1'>
-										<input
-											className='mr-2'
-											type='checkbox'
-											name='technology'
-											value={tech}
-											id={tech}
-											checked={techFilter.has(tech as technologiesEnum)}
-											onChange={onTechChange}
-										/>
-										{tech}
-									</label>
-								)
-							})}
-						</fieldset>
-					)}
-				</div>
-			</div>
+			<ProjectFilters
+				handlers={{ onRoleChange, onTechChange, onTypeChange }}
+				states={{ techFilter, roleFilter, typeFilter }}
+			/>
 			<div className='w-full px-5 shadow-main shadow-font rounded-xl backdrop-blur-lg md:p-10'>
 				{projects.map((project: any) => (
 					<StoryblokComponent blok={project} key={project._uid} />
---
+++ src/PortfolioModule/components/ProjectsFilters/index.tsx
@@ -0,0 +1,124 @@
+import {
+	LangEnum,
+	DevEnum,
+	TechEnum
+} from '@/Global/components/SvgProvider/SvgProvider'
+import { Dispatch, SetStateAction, useState } from 'react'
+
+export type technologiesEnum = LangEnum & DevEnum & TechEnum
+
+export enum role {
+	any = 'any',
+	dev = 'developer',
+	main = 'maintenance',
+	sup = 'support'
+}
+
+export enum projType {
+	any = 'any',
+	commercial = 'commercial',
+	side = 'side'
+}
+
+type Props = {
+	handlers: {
+		onRoleChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
+		onTechChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+		onTypeChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
+	}
+	states: {
+		roleFilter: role
+		typeFilter: projType
+		techFilter: Set<TechEnum>
+	}
+}
+const ProjectFilters: React.FC<Props> = ({ handlers, states }) => {
+	const [isTechInputOpen, setTechInputOpen] = useState(false)
+	const toggleTechInput = () => setTechInputOpen(prev => !prev)
+	// @ts-ignore next line
+	const technologies: technologiesEnum[] = [
+		...Object.values(LangEnum),
+		...Object.values(DevEnum),
+		...Object.values(TechEnum)
+	]
+	const labelStyles = 'flex justify-between p-1 m-0.5 text-center md:w-2/5'
+	const selectStyles =
+		'border border-font outline-0 w-1/2 mx-1 px-2 bg-background'
+	return (
+		<div className='sticky top-0 flex flex-col mb-5 px-5 py-2 w-full shadow-main shadow-font rounded-xl backdrop-blur-lg md:top-16 md:px-10 z-10'>
+			<div className='flex flex-col w-full md:flex-row md:items-center md:justify-around'>
+				<label htmlFor='work' className={labelStyles}>
+					Type:
+					<select
+						className={selectStyles}
+						onChange={handlers.onTypeChange}
+						value={states.typeFilter}
+						name='work'
+						id='work'
+					>
+						<option value={projType.any}>{projType.any}</option>
+						<option value={projType.commercial}>{projType.commercial}</option>
+						<option value={projType.side}>{projType.side}</option>
+					</select>
+				</label>
+				<label htmlFor='role' className={labelStyles}>
+					Role:
+					<select
+						className={selectStyles}
+						onChange={handlers.onRoleChange}
+						value={states.roleFilter}
+						name='role'
+						id='role'
+					>
+						{Object.values(role).map((role: string) => (
+							<option value={role} key={`role-${role}`}>
+								{role}
+							</option>
+						))}
+					</select>
+				</label>
+			</div>
+			<div className='relative'>
+				<button
+					className='w-full border-font border-2 py-1 bg-background text-center'
+					onClick={toggleTechInput}
+				>
+					{isTechInputOpen ? (
+						'Search'
+					) : (
+						<>
+							Tech Stack:{' '}
+							{states.techFilter.size > 0
+								? states.techFilter.size === 1
+									? states.techFilter.values().next().value
+									: states.techFilter.size
+								: 'all'}
+						</>
+					)}
+				</button>
+				{isTechInputOpen && (
+					<fieldset className='absolute top-[105%] left-0 right-0 bg-background text-center p-3 grid grid-cols-1 sm:text-left sm:gap-2 sm:grid-cols-3 lg:grid-cols-5'>
+						{technologies.map((tech: technologiesEnum) => {
+							return (
+								<label key={`tech-${tech}`} htmlFor={tech} className='m-1'>
+									<input
+										className='mr-2'
+										type='checkbox'
+										name='technology'
+										value={tech}
+										id={tech}
+										checked={states.techFilter.has(tech as technologiesEnum)}
+										onChange={handlers.onTechChange}
+									/>
+									{tech}
+								</label>
+							)
+						})}
+					</fieldset>
+				)}
+			</div>
+		</div>
+	)
+}
+
+export default ProjectFilters
