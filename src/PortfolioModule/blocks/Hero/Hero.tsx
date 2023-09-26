27d94a12f47fd30e66df98278b04e914e7935ea8
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2023-09-26 23:20:24 +0200

    Add smooth scroll to hero CTA

--- src/PortfolioModule/blocks/Hero/Hero.tsx
+++ src/PortfolioModule/blocks/Hero/Hero.tsx
@@ -6,6 +6,7 @@ import RichText from '@/Global/components/RIchText/RichText'
 
 import styles from './hero.module.scss'
 import { HeroStoryblok } from '@/types/component-types-sb'
+import { scrollToID } from '@/PortfolioModule/components/Navigation/Navigation'
 // find a way to import glb files nextjs
 /* import dynamic from 'next/dynamic'
 const Avatar = dynamic(
@@ -25,6 +26,13 @@ const Hero: React.FC<Props> = ({ blok }) => {
 		setIsActive(true)
 		setTimeout(() => setIsActive(false), 5000)
 	}
+	const handleClick = (
+		e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+		id: string
+	) => {
+		e.preventDefault()
+		scrollToID(id)
+	}
 	return (
 		<div className='max-w-5xl mx-5 md:justify-self-start md:w-3/4 '>
 			<div className='flex flex-col relative p-7 pb-20 md:px-14 md:pt-10 justify-center rounded-lg backdrop-blur-lg'>
@@ -36,6 +44,7 @@ const Hero: React.FC<Props> = ({ blok }) => {
 				</div>
 				<div className='flex items-center justify-end border-2 border-font bg-secondarybg px-6 py-3 rounded-lg absolute -bottom-8 right-1/2 scale-75 translate-x-1/2 -rotate-12 md:-bottom-5 md:right-1/4 md:scale-95'>
 					<Link
+						onClick={e => handleClick(e, '#contact')}
 						href={`#${cta_link.anchor}`}
 						className='inline-block relative py-1 px-9 bg-background uppercase text-center no-underline text-xl leading-9 font-bold tracking-wide shadow-btnBlack hover:shadow-btnGreen duration-700 hover:rotate-45 hover:scale-125'
 					>
