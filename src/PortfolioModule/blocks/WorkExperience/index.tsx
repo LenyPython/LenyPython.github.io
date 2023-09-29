ecebd6bd9ef8026eae20d49d977b03b8d9aee40b
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2023-09-29 23:10:46 +0200

    Add work experience styling

--- src/PortfolioModule/blocks/WorkExperience/index.tsx
+++ src/PortfolioModule/blocks/WorkExperience/index.tsx
@@ -23,9 +23,9 @@ const WorkExperience: React.FC<Props> = ({ blok, odd }) => {
 			) : (
 				<span className='absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 w-4 h-4 bg-font rounded-full'></span>
 			)}
-			<h3>{company}</h3>
-			<h4>{role}</h4>
-			<p>
+			<h3 className='text-xl font-bold md:text-3xl'>{company}</h3>
+			<h4 className='text-md font-medium underline my-2 md:text-xl'>{role}</h4>
+			<p className='mb-2 font-bold'>
 				{start} - {end}
 			</p>
 			<RichText html={renderRichText(description)} />
